b16f59fdb78eb8b075dfbe25bdf3de6f965edbc8
yitinbai <1058792734@qq.com>
2022-12-16 13:32:00 +0800

    增加编译后的产物,方便在mac上跳转代码

---
+++ src/build.h
@@ -0,0 +1,8 @@
+/* This file is auto-generated, edit at your own risk.  */
+#ifndef BUILD_H
+#define BUILD_H
+#undef VERSION
+#define VERSION "-03d573bc"
+#define HAVE_PROCESS_VM
+#define HAVE_SECCOMP_FILTER
+#endif /* BUILD_H */
